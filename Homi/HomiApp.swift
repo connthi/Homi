98c9a50ba616a5b564774b43d2af422a202ee2c2
Phong Tang <tangphong06@gmail.com>
2025-11-15 17:06:17 -0800

    Initial Commit

---
+++ Homi/HomiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomiApp.swift
+//  Homi
+//
+//  Created by Phong Tang on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HomiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
